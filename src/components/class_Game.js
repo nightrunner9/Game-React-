4f82fcae566498451337b3a804cf4d5bde5a011a
Dmitriy Samarin <dsamarin@solit-clouds.ru>
2021-08-10 18:33:59 +0300

    init : start task

---
+++ src/components/class_Game.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import Board from './class_Board';
+import calculateWinner from './function_calculateWinner';
+
+
+class Game extends React.Component {
+  constructor (props) {
+    super (props) ;
+    this.state = {
+      history : [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+      stepNumber: 0,
+      defaultClassName: "square"    
+    };
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentDidUpdate () {
+
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);         
+    const current = history [history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  render () {
+    const colorChange = this.state.defaultClassName
+    const history = this.state.history;
+    const current = history [this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const btn_class = move === 0 ? 'step_start' : move % 2 != 0 ? 'step_red' : move % 2 == 0 ? 'step_blue' :  '';
+      const desc = move ?
+        'back to step #' + move : 
+        'back to start';
+        return (
+          <li key = {move}>
+              <button 
+              className={`buttons ${btn_class}`}
+              onClick={() => this.jumpTo(move)}> 
+              {desc} 
+              </button>
+          </li>
+        )
+    })
+
+    let status;
+    if (winner) {
+      status = 'Player '+ winner + ' Win !';
+    } else {
+      status = 'next step: ' +  (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board                                                 
+            colorChange={colorChange} //здесь проп 
+            squares={current.squares}
+            onClick={(i)=> this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game
---
+++ src/components/function_square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Square (props)  {
+    return (
+      <button 
+        className={`square ${props.classSquare}`}
+        onClick={props.onClick}
+      >
+        {props.value}
+      </button>
+    );
+}
+
+export default Square
